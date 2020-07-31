9d5741bb537e3a2bfca45c05d46b4c262fee7650
VladOst648 <ostaschovvlad@yandex.ru>
2020-07-31 10:14:55 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public  static  boolean MaySleep(boolean weekday, boolean vacation) {
+        if ((weekday = false) || (vacation = true)) return true;
+        return false;
+    }
+}
